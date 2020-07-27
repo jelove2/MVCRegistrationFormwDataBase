f79f9ffe8a850197d8e944343f46705a03774862
Jacob Love <jakeimlovin@gmail.com>
2020-07-26 21:02:02 -0700

    IMporting Proj

---
+++ UserRegistrationMVC/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistrationMVC.Models;
+
+namespace UserRegistrationMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult AddOrEdit(int id=0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(User userModel)
+        {
+            using(NewDataBase dbModel = new NewDataBase())
+            {
+                if (dbModel.Users.Any( x => x.Username == userModel.Username))
+                {
+                    ViewBag.DuplicateMessage = "Username already exist.";
+                    return View("AddOrEdit", userModel);
+                }
+                dbModel.Users.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+            return View("AddOrEdit", new User());
+        }
+
+    }
+}
